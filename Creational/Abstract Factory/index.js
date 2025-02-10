4092186315bd43d8c826dc95d6ac85f9d479e665
unknown <ayubfmohammed@gmail.com>
2025-02-09 21:54:47 -0700

    Abstract factory

--- Creational/Abstract Factory/index.js
+++ Creational/Abstract Factory/index.js
@@ -1,1 +1,72 @@
-console.log("Hello world");
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Test = /** @class */ (function () {
+    function Test(fileName, filePath, size, type, fileExtension) {
+        this.fileName = fileName;
+        this.filePath = filePath;
+        this.size = size;
+        this.type = type;
+        this.fileExtension = fileExtension;
+    }
+    return Test;
+}());
+// Step 2: Concrete File classes
+var Video = /** @class */ (function (_super) {
+    __extends(Video, _super);
+    function Video(fileName, filePath, size) {
+        return _super.call(this, fileName, filePath, size, "video", "mp4") || this;
+    }
+    Video.prototype.getFileName = function () {
+        return "Video File: ".concat(this.fileName, ".").concat(this.fileExtension);
+    };
+    return Video;
+}(Test));
+var Doc = /** @class */ (function (_super) {
+    __extends(Doc, _super);
+    function Doc(fileName, filePath, size) {
+        return _super.call(this, fileName, filePath, size, "document", "pdf") || this;
+    }
+    Doc.prototype.getFileName = function () {
+        return "Document File: ".concat(this.fileName, ".").concat(this.fileExtension);
+    };
+    return Doc;
+}(Test));
+// Step 4: Concrete Factories
+var VideoFactory = /** @class */ (function () {
+    function VideoFactory() {
+    }
+    VideoFactory.prototype.createFile = function (fileName, filePath, size) {
+        return new Video(fileName, filePath, size);
+    };
+    return VideoFactory;
+}());
+var DocumentFactory = /** @class */ (function () {
+    function DocumentFactory() {
+    }
+    DocumentFactory.prototype.createFile = function (fileName, filePath, size) {
+        return new Doc(fileName, filePath, size);
+    };
+    return DocumentFactory;
+}());
+// Step 5: Client Code (Usage)
+function clientCode(factory) {
+    var file = factory.createFile("example", "/path/to/file", 1024);
+    console.log(file.getFileName());
+}
+// Create a Video file
+clientCode(new VideoFactory()); // Output: Video File: example.mp4
+// Create a Document file
+clientCode(new DocumentFactory()); // Output: Document File: example.pdf
--- Creational/Abstract Factory/index.ts
+++ Creational/Abstract Factory/index.ts
@@ -1,1 +1,68 @@
-console.log("Hello world") ; 
+abstract class Test {
+  protected fileName: string;
+  protected filePath: string;
+  protected size: number;
+  protected type: string;
+  protected fileExtension: string;
+
+  constructor(fileName: string, filePath: string, size: number, type: string, fileExtension: string) {
+    this.fileName = fileName;
+    this.filePath = filePath;
+    this.size = size;
+    this.type = type;
+    this.fileExtension = fileExtension;
+  }
+
+  abstract getFileName(): string;
+}
+
+// Step 2: Concrete File classes
+class Video extends Test {
+  constructor(fileName: string, filePath: string, size: number) {
+    super(fileName, filePath, size, "video", "mp4");
+  }
+
+  getFileName(): string {
+    return `Video File: ${this.fileName}.${this.fileExtension}`;
+  }
+}
+
+class Doc extends Test {
+  constructor(fileName: string, filePath: string, size: number) {
+    super(fileName, filePath, size, "document", "pdf");
+  }
+
+  getFileName(): string {
+    return `Document File: ${this.fileName}.${this.fileExtension}`;
+  }
+}
+
+// Step 3: Abstract Factory
+interface FileFactory {
+  createFile(fileName: string, filePath: string, size: number): Test ;
+}
+
+// Step 4: Concrete Factories
+class VideoFactory implements FileFactory {
+  createFile(fileName: string, filePath: string, size: number): Test {
+    return new Video(fileName, filePath, size);
+  }
+}
+
+class DocumentFactory implements FileFactory {
+  createFile(fileName: string, filePath: string, size: number): Test {
+    return new Doc(fileName, filePath, size);
+  }
+}
+
+// Step 5: Client Code (Usage)
+function clientCode(factory: FileFactory) {
+  const file = factory.createFile("example", "/path/to/file", 1024);
+  console.log(file.getFileName());
+}
+
+// Create a Video file
+clientCode(new VideoFactory());  // Output: Video File: example.mp4
+
+// Create a Document file
+clientCode(new DocumentFactory());  // Output: Document File: example.pdf
